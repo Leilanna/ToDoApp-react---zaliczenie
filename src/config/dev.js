3039b617b7c9435a99eea65c249633720ee9fb87
Michal Gorny <michal.gorny@nexpertis.pl>
2020-05-17 22:14:42 +0200

    Init commit

---
+++ src/config/dev.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyAMomU-tRHb9j6mggP2qZf8MzYmhWkGzps",
+  authDomain: "test-a4a5e.firebaseapp.com",
+  databaseURL: "https://test-a4a5e.firebaseio.com/",
+  projectId: "test-a4a5e",
+  storageBucket: "test-a4a5e.appspot.com",
+  messagingSenderId: "750399659188"
+}
